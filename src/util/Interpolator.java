1186aab4741d42d82aa2a87b0ab43b4112a88037
Paul Stahr <paul.stahr@gmx.de>
2022-01-27 18:55:36 +0100

    little performance optimizations

--- src/util/Interpolator.java
+++ src/util/Interpolator.java
@@ -17,14 +17,18 @@ public class Interpolator {
         if (z + 1 == depth)   {--z;zMod = 255;}
         int index = ((z * height) + y) * width + x;
         float result = 0;
+        int xModI = 0x100 - xMod;
+        int yModI = 0x100 - yMod;
+        int zModI = 0x100 - zMod;
+        int xModD = 2 * xMod - 0x100;
+        int yModD = 2 * yMod - 0x100;
+        int zModD = 2 * zMod - 0x100;
         for (int i = 0; i < 8; ++i)
         {
             int m0 =  i % 2;
             int m1 = (i / 2) % 2;
-            int m2 = ((i / 4) % 2);
-            long multiply  = (0x100 - xMod + m0 * (2 * xMod - 0x100));
-                 multiply *= (0x100 - yMod + m1 * (2 * yMod - 0x100));
-                 multiply *= (0x100 - zMod + m2 * (2 * zMod - 0x100));
+            int m2 = (i / 4) % 2;
+            long multiply  = (xModI + m0 * xModD) * (yModI + m1 * yModD) * (zModI + m2 * zModD);
             float dat = data[index + ((m2 * height) + m1) * width + m0];
             if (dat < 0) {dat -= Integer.MIN_VALUE; dat += Integer.MAX_VALUE;}
             result += dat * multiply;
@@ -48,14 +52,19 @@ public class Interpolator {
         if (z + 1 == depth)   {--z;zMod = 255;}
         int index = ((z * height) + y) * width + x;
         float result = 0;
+        int xModI = 0x100 - xMod;
+        int yModI = 0x100 - yMod;
+        int zModI = 0x100 - zMod;
+        int xModD = 2 * xMod - 0x100;
+        int yModD = 2 * yMod - 0x100;
+        int zModD = 2 * zMod - 0x100;
+
         for (int i = 0; i < 8; ++i)
         {
             int m0 =  i % 2;
             int m1 = (i / 2) % 2;
-            int m2 = ((i / 4) % 2);
-            long multiply  = (0x100 - xMod + m0 * (2 * xMod - 0x100));
-                 multiply *= (0x100 - yMod + m1 * (2 * yMod - 0x100));
-                 multiply *= (0x100 - zMod + m2 * (2 * zMod - 0x100));
+            int m2 = (i / 4) % 2;
+            long multiply  = (xModI + m0 * xModD) * (yModI + m1 * yModD) * (zModI + m2 * zModD);
             float dat = data[index + ((m2 * height) + m1) * width + m0];
             result += dat * multiply;
         }
